9dc42f937084d46606fff0ebfa724ad06dcdbb8a
satish <xsatishtamarkar@moneytap.com>
2021-02-04 11:46:18 +0530

    export routes

--- lib/flutter_common_poc.dart
+++ lib/flutter_common_poc.dart
@@ -1,7 +1,10 @@
 library flutter_common_poc;
 
+import 'package:flutter_common_poc/src/routes.dart';
+
 export 'src/home_page/home.dart' show HomePage;
 export 'src/screens/screen1.dart' show Screen1;
 export 'src/home_page/init_app.dart' show InitApp;
 export 'src/di/di.dart' show Injector;
 export 'src/webservices/base_request.dart' show BaseRequest;
+export 'src/routes.dart' show Routes;
